9b7abda9161864a5276336cc3b284c1c25b2cd9d
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-10 16:57:08 +0100

    Started designing the NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <NavBar/>
+    </>
   );
 }
 
