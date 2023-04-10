8e4c98daa0086d759f198b6297f1f48bdd45267c
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-10 17:46:19 +0100

    Started the Explorer Component

--- src/Components/Explorer/Explorer.jsx
+++ src/Components/Explorer/Explorer.jsx
@@ -4,7 +4,9 @@ import "./Explorer.css";
 const Explorer = () => {
   return (
     <div className="explorer-container">
-      <div className="explorer-search">Explorer</div>
+      <div className="explorer-search">
+        <h5 className="explorer-text">Match Chain Explorer</h5>
+      </div>
     </div>
   );
 };
