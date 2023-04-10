62057b042d6fb63992fccff5ddc941b1e3cb150f
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-10 17:48:46 +0100

    Started the Explorer Component

--- src/Components/Explorer/Explorer.jsx
+++ src/Components/Explorer/Explorer.jsx
@@ -6,6 +6,11 @@ const Explorer = () => {
     <div className="explorer-container">
       <div className="explorer-search">
         <h5 className="explorer-text">Match Chain Explorer</h5>
+        <input
+          type="text"
+          placeholder="Search by address, token symbol, name, transaction hash, or block number"
+        />
+        <button></button>
       </div>
     </div>
   );
