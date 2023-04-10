92e0bd97d65c346a90e9586141a3d1d152f010b9
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-10 19:15:14 +0100

    Started the Explorer Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Explorer from './Components/Explorer/Explorer';
+import LiveData from './Components/LiveData/LiveData';
 
 function App() {
   return (
     <>
       <NavBar/>
       <Explorer/>
+      <LiveData/>
     </>
   );
 }
--- src/Components/LiveData/LiveData.jsx
+++ src/Components/LiveData/LiveData.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import transactionImage from "../../Assets/images/icon_transaction.png";
+import blockImage from "../../Assets/images/icon_block.png";
 import "./LiveData.css";
 const LiveData = () => {
-  return <div>LiveData</div>;
+  return (
+    <>
+      <div className="live-data-container">
+        <div className="blocks-data">
+          <div className="transactions">
+            <div className="image-text">
+              <img src={transactionImage} alt="" />
+              <div>
+                <h5 className="title">TRANSACTIONS</h5>
+                <p>646</p>
+              </div>
+            </div>
+            <div className="gas-fee">
+              <h5 className="title">GAS PRICE</h5>
+              <p>Gwei</p>
+            </div>
+          </div>
+          <hr />
+          <div className="transactions">
+            <div className="image-text">
+              <img src={blockImage} alt="" />
+              <div>
+                <h5 className="title">LATEST BLOCK</h5>
+                <p>406,805</p>
+              </div>
+            </div>
+            <div className="gas-fee">
+              <h5 className="title">WALLET ADDRESS</h5>
+              <p>123</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LiveData;
