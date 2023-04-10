39e5164c2f462f5b7aae5a9acc04f9b2196f142a
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-10 17:21:11 +0100

    Started the Explorer Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import Explorer from './Components/Explorer/Explorer';
 
 function App() {
   return (
     <>
       <NavBar/>
+      <Explorer/>
     </>
   );
 }
---
+++ src/Components/Explorer/Explorer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Explorer.css";
+
+const Explorer = () => {
+  return <div>Explorer</div>;
+};
+
+export default Explorer;
