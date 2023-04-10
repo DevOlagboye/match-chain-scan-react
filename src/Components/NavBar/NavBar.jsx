882371594d50ea3ba153fe33c62fe86d3b592d0c
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-10 17:07:01 +0100

    Started designing the NavBar Component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import matchLabs from "../../Assets/images/match_logo.svg";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <>
       <header className="header-container">
-        <nav></nav>
+        <nav>
+          <div className="logo">
+            <a href="##">
+              <img src={matchLabs} alt="" />
+            </a>
+          </div>
+          <ul className="nav-items">
+            <li>
+              <a href="##">Blockchain</a>
+            </li>
+            <li>
+              <a href="##">Tokens</a>
+            </li>
+            <li>
+              <a href="##">APIs</a>
+            </li>
+            <li>
+              <a href="##">Matchain</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
