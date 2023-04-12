f542bfbb16b2bada43c09b975480934e3ba7077e
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-12 12:18:26 +0100

    Started working on the LiveBlock Component

--- src/Components/LiveBlock/LiveBlock.jsx
+++ src/Components/LiveBlock/LiveBlock.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import transactionImage from "../../Assets/images/icon_transaction.png";
+import blockImage from "../../Assets/images/icon_block.png";
+import "./LiveBlock.css";
 
 const LiveBlock = () => {
-  return <div>LiveBlock</div>;
+  return (
+    <div className="live-block-container">
+      <div className="blocks-transaction">
+        <div className="latest-block">
+          <div className="image-text">
+            <img src={blockImage} alt="" />
+            <h5>Latest Blocks</h5>
+          </div>
+          <hr className="latest-block-hr" />
+        </div>
+        <div className="latest-transaction">
+          <div className="image-text">
+            <img src={transactionImage} alt="" />
+            <h5>Latest Transactions</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LiveBlock;
