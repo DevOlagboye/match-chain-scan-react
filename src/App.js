1786800d0605b7f055d6378118d24a0602932a3f
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-13 17:21:31 +0100

    Removing some unused elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Explorer from "./Components/Explorer/Explorer";
 import LiveData from "./Components/LiveData/LiveData";
-import { WalletContext, BalanceContext, TranSactionsContext } from "./Context/WalletContext";
+import {
+  WalletContext,
+  BalanceContext,
+  TranSactionsContext,
+} from "./Context/WalletContext";
 import { useState } from "react";
 import LiveBlock from "./Components/LiveBlock/LiveBlock";
 import Footer from "./Components/Footer/Footer";
@@ -13,16 +16,16 @@ function App() {
   const [balance, setBalance] = useState(null);
   const [tranLists, setTranLists] = useState([]);
   return (
-    <TranSactionsContext.Provider value={[tranLists, setTranLists]}> 
-    <BalanceContext.Provider value={[balance, setBalance]}>
-      <WalletContext.Provider value={[wallet, setWallet]}>
-        <NavBar />
-        <Explorer />
-        <LiveData />
-        <LiveBlock />
-        <Footer/>
-      </WalletContext.Provider>{" "}
-    </BalanceContext.Provider>
+    <TranSactionsContext.Provider value={[tranLists, setTranLists]}>
+      <BalanceContext.Provider value={[balance, setBalance]}>
+        <WalletContext.Provider value={[wallet, setWallet]}>
+          <NavBar />
+          <Explorer />
+          <LiveData />
+          <LiveBlock />
+          <Footer />
+        </WalletContext.Provider>{" "}
+      </BalanceContext.Provider>{" "}
     </TranSactionsContext.Provider>
   );
 }
